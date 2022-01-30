9f7a2e5887a894d8b8fb17350976a2ae3deb01d5
Jonathan Giles <jonathangiles@fastmail.fm>
2022-01-30 12:50:14 -0500

    Rename project and executable for more consistency

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-~/tools/openocd-gd/bin/openocd -f interface/gd-link.cfg -f target/gd32vf103.cfg -c "init; halt;" -c "program ./.build/GD32VF103C_xxSPL.elf verify; reset; exit;"
+~/tools/openocd-gd/bin/openocd -f interface/gd-link.cfg -f target/gd32vf103.cfg -c "init; halt;" -c "program ./.build/GD32VF103C_START_RTTHREAD_MK.elf verify; reset; exit;"
